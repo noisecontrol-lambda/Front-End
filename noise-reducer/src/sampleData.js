959cdcf47a15719e8b1ca37db1a5b373e7d2de6e
Greg Johnston <GCJohnston91@gmail.com>
2019-07-29 16:30:12 -0400

    sample data created for components

---
+++ noise-reducer/src/sampleData.js
@@ -0,0 +1,16 @@
+const sampleTeacher = [
+  {
+    "id": 0,
+    "username": "greg",
+    "email": "greg@example.com",
+    "firstName": "Greg",
+    "lastName": "Johnston",
+    "title": "Mr.",
+    "theme": "forest",
+    "intake": null,
+    "classes": []
+  },
+
+]
+
+export default sampleTeacher;
