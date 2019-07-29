6712adee78f40c38ce0b1597be146165dea8895b
avni <avni@hellorosey.com>
2019-07-29 18:32:36 +0200

    project set up plus basic login form creation

---
+++ noise-control-app/src/components/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <h1>Welcome to back to Noise Control</h1>
+      <h3>Log in here to see your classrooms.</h3>
+      <p>
+        Need to create an account? <span>Start here.</span>
+      </p>
+      <img src="./img/ian-parker-JRhVvF5VHG4-unsplash.jpg" alt="" />
+      <form className="login-form">
+        <input type="fname" name="fname" placeholder="First name goes here." />
+
+        <input type="lname" name="lname" placeholder="Last name goes here." />
+
+        <input type="email" name="email" placeholder="Email goes here." />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
