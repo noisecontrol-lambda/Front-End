272373cd5d53bf8556efff2ccdb8d0539e060bf3
Greg Johnston <GCJohnston91@gmail.com>
2019-07-30 17:49:44 -0400

    Class component wiring DONE

--- noise-control-app/src/components/Class.js
+++ noise-control-app/src/components/Class.js
@@ -1,53 +1,61 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Button } from 'semantic-ui-react'
+import ClassButton from './ClassButton'
 
 
 function Class (props) {
   props = props.teacher
   const [id, setID] = useState(0)
 
+  const [classes, setClasses] = useState([])
+
+
   const classSelector = event => {
     setID(event.target.value)
+    console.log(id);
+
   }
 
   const [data, setData] = useState([])
 
-
-  const [teacher, setTeacher] = useState()
-  const [currentClass, setCurrentClass] = useState()
-
   useEffect(() => {
     axios.get(`https://noise-controller-backend.herokuapp.com/api/classes`)
       .then(res => {
+        setClasses(res.data)
         setData(res.data[`${id}`])
       })
   },[id])
 
 
   return (
-  <div className="class-page">
-    <Button onClick={classSelector} value='0'>Class 1</Button>
-    <Button onClick={classSelector} value='1'>Class 2</Button>
-    <Button onClick={classSelector} value='2'>Class 3</Button>
-    <div className="class-stats-wrapper">
-      <div className="class-stats">
-        <p>
-          Class Name: {props.title} {props.lastName}
-        </p>
-        <p>
-          Class Grade : {props.classes[`${id}`].grade}
-        </p>
-        <p>
-          Class Size : {props.classes[`${id}`].size}
-        </p>
-        <p>
-          Current Streak : {props.classes[`${id}`].streak}
-        </p>
+    <div className="class-page">
+      { classes.map((iClass, i) =>
+        <ClassButton
+          key={i}
+          name={iClass.name}
+          onClick={classSelector}
+          value={`${i}`}
+        />)}
+        <div className="class-stats-wrapper">
+          <div className="class-stats">
+            <p>
+              Welcome back {props.title} {props.lastName}!
+            </p>
+            <p>
+              Class Grade : {data.grade}
+            </p>
+            <p>
+              Class Size : {data.numberOfKids}
+            </p>
+            <p>
+              Current Streak : {data.streak}
+            </p>
+          </div>
+        </div>
+        <Button>Begin Safari!</Button>
       </div>
-    </div>
-    <Button>Begin Safari!</Button>
-  </div>
-  )
-}
+    )
+  }
+
 export default Class
