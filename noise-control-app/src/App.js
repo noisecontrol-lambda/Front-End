1203fd81bb9f211d5dbe334b20f09d4b8a60cd4a
Greg Johnston <GCJohnston91@gmail.com>
2019-07-30 12:33:29 -0400

    Init

--- noise-control-app/src/App.js
+++ noise-control-app/src/App.js
@@ -4,10 +4,16 @@ import OnboardingWelcome from "./components/OnboardingWelcome";
 import OnboardingBasic from "./components/OnboardingBasic";
 import OnboardingIntake from "./components/OnboardingIntake";
 import OnboardingPreferences from "./components/OnboardingPreferences";
+import Class from "./components/Class";
+import sampleData from "./sampleData"
 
 import WelcomePage from "./components/WelcomePage";
 
+
 import "./App.scss";
+
+const teacher = sampleData;
+console.log(teacher);
 
 function App() {
   return (
@@ -18,6 +24,7 @@ function App() {
       <OnboardingBasic />
       <OnboardingIntake />
       <OnboardingPreferences />
+      <Class teacher={teacher} />
     </div>
   );
 }
