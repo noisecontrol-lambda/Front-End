40cf8a2708652877bae1313e93226f24c311d35e
Christopher Pupo <chrispupo@Christophers-iMac.local>
2019-07-31 15:51:43 -0400

    added divs with ocean and jungle backgrounds

---
+++ noise-control-app/src/components/safariComponent/jungle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CartoonJungle from '../../assets/jungle animals/cartoon-jungle-background_490205.jpg'
+
+export default function Jungle () {
+
+    return (
+      <div className='jungle-container'>
+       <img src={CartoonJungle} alt='cartoon jungle' />
+    </div>
+    )
+  } 
+  
---
+++ noise-control-app/src/components/safariComponent/ocean.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CartoonOcean from '../../assets/jungle animals/ai-cartoon-cute-diving-ocean-world-illustration-background-heypik-6DU43K8.jpg'
+
+export default function Ocean () {
+
+    return (
+      <div className='Ocean-container'>
+       <img src={require ('../../assets/jungle animals/ai-cartoon-cute-diving-ocean-world-illustration-background-heypik-6DU43K8.jpg')} />
+    </div>
+    )
+  } 
