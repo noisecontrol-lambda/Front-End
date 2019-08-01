b0c6b17ba5c91c3bf7c10cd6cfc7623eb8b9e286
avni <avni@hellorosey.com>
2019-08-01 15:50:09 +0200

    fixed styling and minor errors

--- noise-control-app/src/components/Login.js
+++ noise-control-app/src/components/Login.js
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
 import { Card, Button } from "semantic-ui-react";
 
-const Login = (props) => {
-  console.log('users data', props.teachers);
+const Login = props => {
+  console.log("users data", props.teachers);
 
   const [user, setUser] = useState({ email: "", password: "" });
 
   function handleChange(event) {
     const updatedUser = { ...user, [event.target.name]: event.target.value };
-    console.log('handle change',
-    event.target.name,
-    event.target.value,
-    'updated user', updatedUser);
+    console.log(
+      "handle change",
+      event.target.name,
+      event.target.value,
+      "updated user",
+      updatedUser
+    );
     setUser(updatedUser);
-    console.log('user', user);
+    console.log("user", user);
   }
 
   const submitHandler = async event => {
-    
     event.preventDefault();
     // props.login(user, props.loginHandler);
     try {
@@ -29,8 +31,7 @@ const Login = (props) => {
   };
 
   return (
-    <Card className="login"
-      onSubmit={submitHandler}>
+    <Card className="login" onSubmit={submitHandler}>
       <h1>Welcome to back to Noise Control</h1>
       <h3>Log in here to see your classrooms.</h3>
       <form className="login-form">
@@ -48,9 +49,7 @@ const Login = (props) => {
           value={user.password}
           onChange={handleChange}
         />
-        <Button className="login-button">
-          Submit
-        </Button>
+        <Button className="login-button">Submit</Button>
       </form>
       <p>
         Need to create an account?{" "}
